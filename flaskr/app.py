1738b4209a89d081a347e92bace43144ee1e1df8
dianabaltazar <diana.baltco@gmail.com>
2021-05-21 08:45:23 -0500

    edit app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,22 +1,23 @@
 # Import Dependencies
 
-import pandas as pd 
-import numpy as np
+from datetime import datetime
 
-from config import PostgresK
+import numpy as np
+import pandas as pd
 import psycopg2
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from config import PostgresK
 
 app = Flask(__name__)
 
 # Create the connection engine
-connection_string = "ympsazqytxgdyd:"+PostgresK+"@ec2-54-152-185-191.compute-1.amazonaws.com:5432/d9pes28e80ohoe"
+connection_string = "ympsazqytxgdyd:"+PostgresK + \
+    "@ec2-54-152-185-191.compute-1.amazonaws.com:5432/d9pes28e80ohoe"
 
 #engine = create_engine("postgres://"+connection_string)
-#engine.table_names()
+# engine.table_names()
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://"+connection_string
 data = SQLAlchemy(app)
 
@@ -24,20 +25,21 @@ data = SQLAlchemy(app)
 # covid = data.Table('coviddatah', data.metadata, autoload= True, autoload_with= data.engine)
 
 
-
 @app.route('/')
 def index():
-    schools = pd.read_sql_table('schoolclosuresh',"postgres://"+connection_string)
-    covid = pd.read_sql_table('coviddatah',"postgres://"+connection_string)
+    schools = pd.read_sql_table(
+        'schoolclosuresh', "postgres://"+connection_string)
+    covid = pd.read_sql_table('coviddatah', "postgres://"+connection_string)
 
     merged = schools.merge(covid, how='inner')
-    # df_join = pd.merge(filtered, filt_school, left_on = “iso_code”, right_on = “iso_code”, how=“left”)
-    schoolsJSON = merged.iloc[0:5,:].to_json(orient="records")
+
+    schoolsJSON = merged.iloc[0:5, :].to_json(orient="records")
     print(schoolsJSON)
     # for i in bla:
     #     print(i)
     """Return the dashboard homepage"""
-    return schoolsJSON 
+    return schoolsJSON
+
 
 if __name__ == "__main__":
     app.run()
