098973ba289705aa75731dc8995c6a3b2231d3b1
dianabaltazar <diana.baltco@gmail.com>
2021-05-21 21:17:14 -0500

    changes on app

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,14 +1,16 @@
 # Import Dependencies
-
-from datetime import datetime
-
-import numpy as np
 import pandas as pd
+import numpy as np
+
+from config import PostgresK
 import psycopg2
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
-from config import PostgresK
+from datetime import datetime
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
@@ -16,30 +18,59 @@ app = Flask(__name__)
 connection_string = "ympsazqytxgdyd:"+PostgresK + \
     "@ec2-54-152-185-191.compute-1.amazonaws.com:5432/d9pes28e80ohoe"
 
-#engine = create_engine("postgres://"+connection_string)
-# engine.table_names()
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://"+connection_string
 data = SQLAlchemy(app)
 
-# schools = data.Table('schoolclosuresh', data.metadata, autoload= True, autoload_with= data.engine)
-# covid = data.Table('coviddatah', data.metadata, autoload= True, autoload_with= data.engine)
+schools = data.Table('schoolclosuresh', data.metadata,
+                     autoload=True, autoload_with=data.engine)
+covid = data.Table('coviddatah', data.metadata,
+                   autoload=True, autoload_with=data.engine)
+
+Session = sessionmaker(bind=data.engine)
 
 
-@app.route('/')
-def index():
-    schools = pd.read_sql_table(
-        'schoolclosuresh', "postgres://"+connection_string)
-    covid = pd.read_sql_table('coviddatah', "postgres://"+connection_string)
+# @app.route('/')
+# def index():
+#     """Return the dashboard homepage"""
+#     return render_template('index.html')
 
-    merged = schools.merge(covid, how='inner')
 
-    schoolsJSON = merged.iloc[0:5, :].to_json(orient="records")
-    print(schoolsJSON)
-    # for i in bla:
-    #     print(i)
-    """Return the dashboard homepage"""
-    return schoolsJSON
+@app.route('/json-data')
+def jsondata():
+    session = Session()
+
+    cols1 = covid.columns
+    results = session.query(cols1.date, cols1.iso_code, cols1.continent,
+                            cols1.location, cols1.total_cases).all()
+
+    results_dict = []
+    for date, iso, continent, loc, cases in results:
+        loop_dict = {}
+        loop_dict["date"] = date
+        loop_dict["iso_code"] = iso
+        loop_dict["continent"] = continent
+        loop_dict["location"] = loc
+        loop_dict["total_cases"] = cases
+        #loop_dict["school_closures"] = school
+
+        results_dict.append(loop_dict)
+
+    # cols2 = schools.columns
+    # results2 = session.query(cols2.country, cols2.iso_code, cols2.date,
+    #                          cols2.school_closures).all()
+
+    # results_dict2 = []
+    # for loc, iso, date, school in results2:
+    #     loop_dict2 = {}
+    #     loop_dict2["country"] = loc
+    #     loop_dict2["iso_code"] = iso
+    #     loop_dict2["date"] = date
+    #     loop_dict2["school_closures"] = school
+
+    #     results_dict2.append(loop_dict2)
+
+    return jsonify(results_dict)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
