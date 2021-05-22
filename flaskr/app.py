00a371f0c2502830e610927656c9fa4947e121b8
dianabaltazar <diana.baltco@gmail.com>
2021-05-21 21:34:23 -0500

    App changes

--- flaskr/app.py
+++ flaskr/app.py
@@ -21,18 +21,16 @@ connection_string = "ympsazqytxgdyd:"+PostgresK + \
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://"+connection_string
 data = SQLAlchemy(app)
 
-schools = data.Table('schoolclosuresh', data.metadata,
-                     autoload=True, autoload_with=data.engine)
-covid = data.Table('coviddatah', data.metadata,
+covid = data.Table('mergedcovid', data.metadata,
                    autoload=True, autoload_with=data.engine)
 
 Session = sessionmaker(bind=data.engine)
 
 
-# @app.route('/')
-# def index():
-#     """Return the dashboard homepage"""
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    """Return the dashboard homepage"""
+    return render_template('index.html')
 
 
 @app.route('/json-data')
@@ -41,33 +39,19 @@ def jsondata():
 
     cols1 = covid.columns
     results = session.query(cols1.date, cols1.iso_code, cols1.continent,
-                            cols1.location, cols1.total_cases).all()
+                            cols1.location, cols1.total_cases, cols1.school_closures).all()
 
     results_dict = []
-    for date, iso, continent, loc, cases in results:
+    for date, iso, continent, loc, cases, school in results:
         loop_dict = {}
         loop_dict["date"] = date
         loop_dict["iso_code"] = iso
         loop_dict["continent"] = continent
         loop_dict["location"] = loc
         loop_dict["total_cases"] = cases
-        #loop_dict["school_closures"] = school
+        loop_dict["school_closures"] = school
 
         results_dict.append(loop_dict)
-
-    # cols2 = schools.columns
-    # results2 = session.query(cols2.country, cols2.iso_code, cols2.date,
-    #                          cols2.school_closures).all()
-
-    # results_dict2 = []
-    # for loc, iso, date, school in results2:
-    #     loop_dict2 = {}
-    #     loop_dict2["country"] = loc
-    #     loop_dict2["iso_code"] = iso
-    #     loop_dict2["date"] = date
-    #     loop_dict2["school_closures"] = school
-
-    #     results_dict2.append(loop_dict2)
 
     return jsonify(results_dict)
 
