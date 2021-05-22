3604b0432ba37c6bc971fbc2d96cbe018bea7c04
dianabaltazar <diana.baltco@gmail.com>
2021-05-22 11:02:56 -0500

    Update barplot.js

--- flaskr/static/js/barplot.js
+++ flaskr/static/js/barplot.js
@@ -9,17 +9,17 @@
 
         locations = new Set(data.map(d => d.location));
         // var locations = response['location'];
-        
+        // console.log(data)     
 
         // var date = response[0]['date'];
         // var total_cases = response[0]['total_cases'];
         // locations = new Set(data.map(d => d.countries))
         
-        // datevalues = Array.from(d3.rollup(data, ([d]) => d.total_cases, d => +d.date, d => d.country))
-        //     .map(([date, data]) => [new Date(date), data])
-        //     .sort(([a], [b]) => d3.ascending(a, b));
-
-        console.log(locations)
+        datevalues = Array.from(d3.rollup(data, ([d]) => d.total_cases, d => +d.date, d => d.location))
+            .map(([date, data]) => [new Date(date), data])
+            .sort(([a], [b]) => d3.ascending(a, b));
+        
+        console.log(datevalues) 
 
         // function rank(value) {
         //     const data = Array.from(locations, name => ({name, value: total_cases(location)}));
@@ -27,6 +27,30 @@
         //     for (let i = 0; i < data.length; ++i) data[i].rank = Math.min(n, i);
         //     return data;
         //   }
+
+        //   rank(name => datevalues[0][1].get(name))
+
+        // keyframes = {
+        //     const keyframes = [];
+        //     let ka, a, kb, b;
+        //     for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
+        //       for (let i = 0; i < k; ++i) {
+        //         const t = i / k;
+        //         keyframes.push([
+        //           new Date(ka * (1 - t) + kb * t),
+        //           rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
+        //         ]);
+        //       }
+        //     }
+        //     keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
+        //     return keyframes;
+        //   }
+
+        // nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.name)
+
+        // prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
+
+        // next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
 
     });
 
