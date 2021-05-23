85b7e66b1466620b4c308519280bbf6b8849d290
dianabaltazar <diana.baltco@gmail.com>
2021-05-22 22:08:04 -0500

    fixing barplot data

--- flaskr/static/js/barplot.js
+++ flaskr/static/js/barplot.js
@@ -61,70 +61,10 @@
         }
 
         nameframes = d3.groups(keyframes().flatMap(([, data]) => data), d => d.locName)
-        console.log(nameframes)
 
+        prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
 
-        /*/console.log(keyframes())
-        
-        // dates
-        //   .forEach(date => {
-        //     let bar = new Map()
-        //     let obj = {}
-        //     data
-        //       .filter(d => d.date == date)
-        //       .forEach(d => {
-        //         bar.set(d.location, d.total_cases)
-        //       })
-        //     obj[date] = bar
-        //     foo.push(obj)
-        //   })
-        
+        next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
 
-        // foo.forEach(d => console.log(d.entries))
-
-        
-
-
-      // }).catch(function(error) {
-      //   console.log(error);
-
-
-      
-        //     var arr = [79, 5, 18, 5, 32, 1, 16, 1, 82, 13];
-        //     var sorted = arr.slice().sort(function(a,b){return b-a})
-        //     var ranks = arr.map(function(v){ return sorted.indexOf(v)+1 });
-        //     console.log(ranks);
-
-        //     let data1 = Array.from(locations, x => ({x, y: y(location)}));
-        //     data1.sort((a, b) => d3.descending(a.y, b.y));
-        //     for (let i = 0; i < data1.length; ++i) data1[i].rank = Math.min(n, i);
-        //     return data1;
-        
-        
-
-
-        //   rank(name => datevalues[0][1].get(location))
-
-        // keyframes = {
-        //     const keyframes = [];
-        //     let ka, a, kb, b;
-        //     for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
-        //       for (let i = 0; i < k; ++i) {
-        //         const t = i / k;
-        //         keyframes.push([
-        //           new Date(ka * (1 - t) + kb * t),
-        //           rank(name => (a.get(location) || 0) * (1 - t) + (b.get(location) || 0) * t)
-        //         ]);
-        //       }
-        //     }
-        //     keyframes.push([new Date(kb), rank(name => b.get(location) || 0)]);
-        //     return keyframes;
-        //   }
-
-        // nameframes = d3.groups(keyframes.flatMap(([, data]) => data), d => d.location)
-
-        // prev = new Map(nameframes.flatMap(([, data]) => d3.pairs(data, (a, b) => [b, a])))
-
-        // next = new Map(nameframes.flatMap(([, data]) => d3.pairs(data)))
 
     });
